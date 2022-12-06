ea9f5228a351a00ba2dcdb76ba8ec57b5ee2c705
Borodin Oleg <olezhka_04@list.ru>
2022-12-06 12:01:28 +0300

    Добавили файл main.py цифры

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 
 def password():
     a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    c = ['1','2','3','4','5','6','7','8','9','0']
     otvet = ''
     for i in range(11):
         otvet += random.choice(a)
