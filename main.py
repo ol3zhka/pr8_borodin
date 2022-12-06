d6d6dd73d740043b39a37685473ffd2002a4d084
Borodin Oleg <olezhka_04@list.ru>
2022-12-06 11:53:25 +0300

    Добавлен файл main.py в верхний репозиторий

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 
 def password():
     a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    b = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     otvet = ''
     for i in range(11):
         otvet += random.choice(a)
